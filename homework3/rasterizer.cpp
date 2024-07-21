648f1614008b85f2ed76d0f58a1a1a7d7ffc0d2b
danxinsihuo <1580362865@qq.com>
2024-07-21 21:32:41 +0800

    作业3、4、5

---
+++ homework3/rasterizer.cpp
@@ -0,0 +1,376 @@
+//
+// Created by goksu on 4/6/19.
+//
+
+#include <algorithm>
+#include "rasterizer.hpp"
+#include <opencv2/opencv.hpp>
+#include <math.h>
+
+rst::pos_buf_id rst::rasterizer::load_positions(const std::vector<Eigen::Vector3f> &positions)
+{
+    auto id = get_next_id();
+    pos_buf.emplace(id, positions);
+
+    return {id};
+}
+
+rst::ind_buf_id rst::rasterizer::load_indices(const std::vector<Eigen::Vector3i> &indices)
+{
+    auto id = get_next_id();
+    ind_buf.emplace(id, indices);
+
+    return {id};
+}
+
+rst::col_buf_id rst::rasterizer::load_colors(const std::vector<Eigen::Vector3f> &cols)
+{
+    auto id = get_next_id();
+    col_buf.emplace(id, cols);
+
+    return {id};
+}
+
+rst::col_buf_id rst::rasterizer::load_normals(const std::vector<Eigen::Vector3f> &normals)
+{
+    auto id = get_next_id();
+    nor_buf.emplace(id, normals);
+
+    normal_id = id;
+
+    return {id};
+}
+
+// Bresenham's line drawing algorithm
+void rst::rasterizer::draw_line(Eigen::Vector3f begin, Eigen::Vector3f end)
+{
+    auto x1 = begin.x();
+    auto y1 = begin.y();
+    auto x2 = end.x();
+    auto y2 = end.y();
+
+    Eigen::Vector3f line_color = {255, 255, 255};
+
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = fabs(dx);
+    dy1 = fabs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+
+    if (dy1 <= dx1)
+    {
+        if (dx >= 0)
+        {
+            x = x1;
+            y = y1;
+            xe = x2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            xe = x1;
+        }
+        Eigen::Vector2i point = Eigen::Vector2i(x, y);
+        set_pixel(point, line_color);
+        for (i = 0; x < xe; i++)
+        {
+            x = x + 1;
+            if (px < 0)
+            {
+                px = px + 2 * dy1;
+            }
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                {
+                    y = y + 1;
+                }
+                else
+                {
+                    y = y - 1;
+                }
+                px = px + 2 * (dy1 - dx1);
+            }
+            //            delay(0);
+            Eigen::Vector2i point = Eigen::Vector2i(x, y);
+            set_pixel(point, line_color);
+        }
+    }
+    else
+    {
+        if (dy >= 0)
+        {
+            x = x1;
+            y = y1;
+            ye = y2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            ye = y1;
+        }
+        Eigen::Vector2i point = Eigen::Vector2i(x, y);
+        set_pixel(point, line_color);
+        for (i = 0; y < ye; i++)
+        {
+            y = y + 1;
+            if (py <= 0)
+            {
+                py = py + 2 * dx1;
+            }
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                {
+                    x = x + 1;
+                }
+                else
+                {
+                    x = x - 1;
+                }
+                py = py + 2 * (dx1 - dy1);
+            }
+            //            delay(0);
+            Eigen::Vector2i point = Eigen::Vector2i(x, y);
+            set_pixel(point, line_color);
+        }
+    }
+}
+
+auto to_vec4(const Eigen::Vector3f &v3, float w = 1.0f)
+{
+    return Vector4f(v3.x(), v3.y(), v3.z(), w);
+}
+
+static bool insideTriangle(int x, int y, const Vector4f *_v)
+{
+    Vector3f v[3];
+    for (int i = 0; i < 3; i++)
+        v[i] = {_v[i].x(), _v[i].y(), 1.0};
+    Vector3f f0, f1, f2;
+    f0 = v[1].cross(v[0]);
+    f1 = v[2].cross(v[1]);
+    f2 = v[0].cross(v[2]);
+    Vector3f p(x, y, 1.);
+    if ((p.dot(f0) * f0.dot(v[2]) > 0) && (p.dot(f1) * f1.dot(v[0]) > 0) && (p.dot(f2) * f2.dot(v[1]) > 0))
+        return true;
+    return false;
+}
+
+static std::tuple<float, float, float> computeBarycentric2D(float x, float y, const Vector4f *v)
+{
+    float c1 = (x * (v[1].y() - v[2].y()) + (v[2].x() - v[1].x()) * y + v[1].x() * v[2].y() - v[2].x() * v[1].y()) / (v[0].x() * (v[1].y() - v[2].y()) + (v[2].x() - v[1].x()) * v[0].y() + v[1].x() * v[2].y() - v[2].x() * v[1].y());
+    float c2 = (x * (v[2].y() - v[0].y()) + (v[0].x() - v[2].x()) * y + v[2].x() * v[0].y() - v[0].x() * v[2].y()) / (v[1].x() * (v[2].y() - v[0].y()) + (v[0].x() - v[2].x()) * v[1].y() + v[2].x() * v[0].y() - v[0].x() * v[2].y());
+    float c3 = (x * (v[0].y() - v[1].y()) + (v[1].x() - v[0].x()) * y + v[0].x() * v[1].y() - v[1].x() * v[0].y()) / (v[2].x() * (v[0].y() - v[1].y()) + (v[1].x() - v[0].x()) * v[2].y() + v[0].x() * v[1].y() - v[1].x() * v[0].y());
+    return {c1, c2, c3};
+}
+
+void rst::rasterizer::draw(std::vector<Triangle *> &TriangleList)
+{
+
+    float f1 = (50 - 0.1) / 2.0;
+    float f2 = (50 + 0.1) / 2.0;
+
+    Eigen::Matrix4f mvp = projection * view * model;
+    for (const auto &t : TriangleList)
+    {
+        Triangle newtri = *t;
+
+        std::array<Eigen::Vector4f, 3> mm{
+            (view * model * t->v[0]),
+            (view * model * t->v[1]),
+            (view * model * t->v[2])};
+
+        // 没有经过P矩阵的,viewspace的顶点坐标
+        std::array<Eigen::Vector3f, 3> viewspace_pos;
+
+        std::transform(mm.begin(), mm.end(), viewspace_pos.begin(), [](auto &v)
+                       { return v.template head<3>(); });
+
+        Eigen::Vector4f v[] = {
+            mvp * t->v[0],
+            mvp * t->v[1],
+            mvp * t->v[2]};
+        // Homogeneous division
+        for (auto &vec : v)
+        {
+            vec.x() /= vec.w();
+            vec.y() /= vec.w();
+            vec.z() /= vec.w();
+        }
+
+        // MV矩阵求逆再转置
+        Eigen::Matrix4f inv_trans = (view * model).inverse().transpose();
+        // n[] is view space normal of the Vertexs
+        Eigen::Vector4f n[] = {
+            inv_trans * to_vec4(t->normal[0], 0.0f),
+            inv_trans * to_vec4(t->normal[1], 0.0f),
+            inv_trans * to_vec4(t->normal[2], 0.0f)};
+
+        // Viewport transformation
+        for (auto &vert : v)
+        {
+            vert.x() = 0.5 * width * (vert.x() + 1.0);
+            vert.y() = 0.5 * height * (vert.y() + 1.0);
+            vert.z() = vert.z() * f1 + f2;
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            // screen space coordinates
+            newtri.setVertex(i, v[i]);
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            // view space normal
+            newtri.setNormal(i, n[i].head<3>());
+        }
+
+        newtri.setColor(0, 148, 121.0, 92.0);
+        newtri.setColor(1, 148, 121.0, 92.0);
+        newtri.setColor(2, 148, 121.0, 92.0);
+
+        // Also pass view space vertice position
+        // viewspace_pos是相机空间的顶点坐标：在光栅化器中需要使用相机空间的顶点坐标对三角形内每个像素点坐标进行插值。插值出来的坐标会送入shader中，并在之后的phong_fragment_shader等过程中作为shader point使用。
+        // 为什么shader point要使用相机空间的顶点坐标，不用经过了mvp矩阵的屏幕空间坐标？ -> 原因是在光照模型中需要用到法向量，法向量只能在相机空间中计算。因为P矩阵和透视除法会改变原来的三角形的z值，经过P矩阵和透视除法后的NDC坐标系中，保留z值只是为了做深度判断，这个z值已经不是原来三角形的z值了，无法在裁剪空间和NDC空间求出法向量。因此因为法向量在相机空间，shader point也就需要在相机空间，这样才能使用光照模型。
+        rasterize_triangle(newtri, viewspace_pos);
+    }
+}
+
+// 三维向量插值,用在color上
+static Eigen::Vector3f interpolate(float alpha, float beta, float gamma, const Eigen::Vector3f &vert1, const Eigen::Vector3f &vert2, const Eigen::Vector3f &vert3, float weight)
+{
+    return (alpha * vert1 + beta * vert2 + gamma * vert3) / weight;
+}
+
+// 二维向量插值,用在texcoords上
+static Eigen::Vector2f interpolate(float alpha, float beta, float gamma, const Eigen::Vector2f &vert1, const Eigen::Vector2f &vert2, const Eigen::Vector2f &vert3, float weight)
+{
+    auto u = (alpha * vert1[0] + beta * vert2[0] + gamma * vert3[0]);
+    auto v = (alpha * vert1[1] + beta * vert2[1] + gamma * vert3[1]);
+
+    u /= weight;
+    v /= weight;
+
+    return Eigen::Vector2f(u, v);
+}
+
+void rst::rasterizer::rasterize_triangle(const Triangle &t, const std::array<Eigen::Vector3f, 3> &view_pos)
+{
+    auto v = t.toVector4();
+    // get the bounding box of current triangle
+    int minX, minY, maxX, maxY;
+    minX = std::min(v[0].x(), std::min(v[1].x(), v[2].x()));
+    maxX = std::max(v[0].x(), std::max(v[1].x(), v[2].x()));
+    minY = std::min(v[0].y(), std::min(v[1].y(), v[2].y()));
+    maxY = std::max(v[0].y(), std::max(v[1].y(), v[2].y()));
+
+    for (int y = minY; y <= maxY; y++)
+    {
+        for (int x = minX; x <= maxX; x++)
+        {
+            if (insideTriangle((float)x + 0.5, (float)y + 0.5, t.v)) // 以像素中心点作为采样点
+            {
+                auto tuple = computeBarycentric2D((float)x + 0.5, (float)y + 0.5, t.v);
+                float alpha = std::get<0>(tuple);
+                float beta = std::get<1>(tuple);
+                float gamma = std::get<2>(tuple);
+                // 深度插值
+                float w_reciprocal = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+                float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
+                z_interpolated *= w_reciprocal;
+
+                if (z_interpolated < depth_buf[get_index(x, y)])
+                {
+                    // 插值的作用:从三个顶点的颜色,坐标,法向量,UV坐标得到整个三角形中每一个像素点的颜色,坐标,法向量,UV坐标
+                    // 颜色插值
+                    auto interpolated_color = interpolate(alpha, beta, gamma, t.color[0], t.color[1], t.color[2], 1);
+                    // 法向量插值
+                    auto interpolated_normal = interpolate(alpha, beta, gamma, t.normal[0], t.normal[1], t.normal[2], 1);
+                    // 纹理坐标插值
+                    auto interpolated_texcoords = interpolate(alpha, beta, gamma, t.tex_coords[0], t.tex_coords[1], t.tex_coords[2], 1);
+                    // 内部点位置插值
+                    auto interpolated_shadingcoords = interpolate(alpha, beta, gamma, view_pos[0], view_pos[1], view_pos[2], 1);
+
+                    // 片元着色,没有去做顶点着色
+                    fragment_shader_payload payload(interpolated_color, interpolated_normal.normalized(), interpolated_texcoords, texture ? &*texture : nullptr);
+                    payload.view_pos = interpolated_shadingcoords;
+                    auto pixel_color = fragment_shader(payload);
+
+                    set_pixel(Vector2i(x, y), pixel_color);      // 设置颜色
+                    depth_buf[get_index(x, y)] = z_interpolated; // 写入深度缓存
+                }
+            }
+        }
+    }
+}
+
+void rst::rasterizer::set_model(const Eigen::Matrix4f &m)
+{
+    model = m;
+}
+
+void rst::rasterizer::set_view(const Eigen::Matrix4f &v)
+{
+    view = v;
+}
+
+void rst::rasterizer::set_projection(const Eigen::Matrix4f &p)
+{
+    projection = p;
+}
+
+/*
+巧用位运算来控制分支执行：
+1.Color是1，Depth是2，换成位表示Color=01，Depth=10
+2.这样只传入Color代表执行第一种分支，只传入Depth代表执行第二种分支，传入Color|Depth代表都执行
+3.第一种分支取第一位判断是否是1，第二种分支取第二位判断，分别判断即可
+ */
+void rst::rasterizer::clear(rst::Buffers buff)
+{
+    if ((buff & rst::Buffers::Color) == rst::Buffers::Color)
+    {
+        std::fill(frame_buf.begin(), frame_buf.end(), Eigen::Vector3f{0, 0, 0});
+    }
+    if ((buff & rst::Buffers::Depth) == rst::Buffers::Depth)
+    {
+        std::fill(depth_buf.begin(), depth_buf.end(), std::numeric_limits<float>::infinity());
+    }
+}
+
+rst::rasterizer::rasterizer(int w, int h) : width(w), height(h)
+{
+    frame_buf.resize(w * h);
+    depth_buf.resize(w * h);
+    MSAA_frame_buf.resize(w * h * 4);
+    MSAA_depth_buf.resize(w * h * 4);
+    texture = std::nullopt;
+}
+
+int rst::rasterizer::get_index(int x, int y)
+{
+    return (height - y) * width + x;
+}
+
+void rst::rasterizer::set_pixel(const Vector2i &point, const Eigen::Vector3f &color)
+{
+    // old index: auto ind = point.y() + point.x() * width;
+    int ind = (height - point.y()) * width + point.x();
+    frame_buf[ind] = color;
+}
+
+void rst::rasterizer::set_vertex_shader(std::function<Eigen::Vector3f(vertex_shader_payload)> vert_shader)
+{
+    vertex_shader = vert_shader;
+}
+
+void rst::rasterizer::set_fragment_shader(std::function<Eigen::Vector3f(fragment_shader_payload)> frag_shader)
+{
+    fragment_shader = frag_shader;
+}
---
+++ homework3/rasterizer.hpp
@@ -0,0 +1,128 @@
+//
+// Created by goksu on 4/6/19.
+//
+
+#pragma once
+
+#include <Eigen/Eigen>
+#include <optional>
+#include <algorithm>
+#include "global.hpp"
+#include "Shader.hpp"
+#include "Triangle.hpp"
+
+using namespace Eigen;
+
+namespace rst
+{
+	enum class Buffers
+	{
+		Color = 1,
+		Depth = 2
+	};
+
+	inline Buffers operator|(Buffers a, Buffers b)
+	{
+		return Buffers((int)a | (int)b);
+	}
+
+	inline Buffers operator&(Buffers a, Buffers b)
+	{
+		return Buffers((int)a & (int)b);
+	}
+
+	enum class Primitive
+	{
+		Line,
+		Triangle
+	};
+
+	/*
+	 * For the curious : The draw function takes two buffer id's as its arguments. These two structs
+	 * make sure that if you mix up with their orders, the compiler won't compile it.
+	 * Aka : Type safety
+	 * */
+	struct pos_buf_id
+	{
+		int pos_id = 0;
+	};
+
+	struct ind_buf_id
+	{
+		int ind_id = 0;
+	};
+
+	struct col_buf_id
+	{
+		int col_id = 0;
+	};
+
+	class rasterizer
+	{
+	public:
+		rasterizer(int w, int h); // 构造一个w*h的frame_buf和depth_buf
+		pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
+		ind_buf_id load_indices(const std::vector<Eigen::Vector3i>& indices);
+		col_buf_id load_colors(const std::vector<Eigen::Vector3f>& colors);
+		col_buf_id load_normals(const std::vector<Eigen::Vector3f>& normals);
+
+		void set_model(const Eigen::Matrix4f& m);
+		void set_view(const Eigen::Matrix4f& v);
+		void set_projection(const Eigen::Matrix4f& p);
+
+		void set_texture(Texture tex) { texture = tex; }
+
+		void set_vertex_shader(std::function<Eigen::Vector3f(vertex_shader_payload)> vert_shader);
+		void set_fragment_shader(std::function<Eigen::Vector3f(fragment_shader_payload)> frag_shader);
+
+		void set_pixel(const Vector2i& point, const Eigen::Vector3f& color);
+
+		void clear(Buffers buff);
+
+		void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type);
+		void draw(std::vector<Triangle*>& TriangleList);
+
+		std::vector<Eigen::Vector3f>& frame_buffer() { return frame_buf; }
+
+	private:
+		void draw_line(Eigen::Vector3f begin, Eigen::Vector3f end);
+
+		void rasterize_triangle(const Triangle& t, const std::array<Eigen::Vector3f, 3>& world_pos);
+
+		int MSAA(int x, int y, const Triangle& t, int n, int m, const std::array<Eigen::Vector3f, 3>& view_pos);
+
+		// VERTEX SHADER -> MVP -> Clipping -> /.W -> VIEWPORT -> DRAWLINE/DRAWTRI -> FRAGSHADER
+
+	private:
+		Eigen::Matrix4f model;
+		Eigen::Matrix4f view;
+		Eigen::Matrix4f projection;
+
+		int normal_id = -1;
+
+		// 位置缓存:map，int->容器，里面存的是3维向量->顶点的位置向量
+		std::map<int, std::vector<Eigen::Vector3f>> pos_buf;
+		// 索引缓存:map，int->容器，存的是一个3维整型向量，代表一个图形的顶点索引，因此一个向量代表一个图形，容器的容量是需要渲染的图形个数
+		std::map<int, std::vector<Eigen::Vector3i>> ind_buf;
+		std::map<int, std::vector<Eigen::Vector3f>> col_buf;
+		std::map<int, std::vector<Eigen::Vector3f>> nor_buf;
+
+		std::optional<Texture> texture;
+
+		std::function<Eigen::Vector3f(fragment_shader_payload)> fragment_shader;
+		std::function<Eigen::Vector3f(vertex_shader_payload)> vertex_shader;
+
+		// 帧缓冲：一个容器，容器里面存的是3维向量->RGB值，容器的索引即屏幕坐标(w,h)展开的值
+		std::vector<Eigen::Vector3f> frame_buf;
+		std::vector<Eigen::Vector3f> MSAA_frame_buf;
+		// 深度缓存
+		std::vector<float> depth_buf;
+		std::vector<float> MSAA_depth_buf;
+		int get_index(int x, int y); // 从二维数组索引展开成一维数组索引
+
+		int width, height;
+
+		int next_id = 0;
+		int get_next_id() { return next_id++; }
+	};
+}
